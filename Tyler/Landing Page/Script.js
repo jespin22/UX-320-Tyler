2aa4341b128e16a1cb37200f26f213b1ebbe756f
medina-eduardo <escaleraeduardo03@gmail.com>
2025-03-02 22:38:11 -0500

    How it works added to LP

--- Tyler/Landing Page/Script.js
+++ Tyler/Landing Page/Script.js
@@ -1,41 +1,27 @@
-// List of images to cycle through
-const images = [
-    "Tyler/Landing Page/Landing Images/CTRL THE  (artsist) /Property 1=SZA.png", 
-    "Tyler/Landing Page/Landing Images/CTRL THE  (artsist) /Property 2=TYLER.png", 
-    "Tyler/Landing Page/Landing Images/CTRL THE  (artsist) /Property 3=DOECHII.png"
+// List of artist images for the slideshow
+const artistImages = [
+    "./Landing Images/CTRL-THE-ARTIST/Property1-SZA.png",
+    "./Landing Images/CTRL-THE-ARTIST/Property2-TYLER.png",
+    "./Landing Images/CTRL-THE-ARTIST/Property3-DOECHII.png"
 ];
 
-// Start at the first image
-let currentImageIndex = 0;
+// Keep track of the current image
+let currentArtistIndex = 0;
 
-// Get the slideshow element
-const slideElement = document.getElementById("slide");
+// Get the image element in the hero section
+const artistImageElement = document.getElementById("artistSlideshow");
 
+// Debugging: Check if the script is running
+console.log("Slideshow script is running");
 
-// Function to change the image
-function changeSlide() {
-    slideElement.classList.remove("slide-animation"); // Reset animation
+// Function to update the image every 3 seconds
+function updateArtistImage() {
+    currentArtistIndex = (currentArtistIndex + 1) % artistImages.length; 
+    artistImageElement.src = artistImages[currentArtistIndex]; 
 
-    setTimeout(() => {
-        currentImageIndex = (currentImageIndex + 1) % images.length;
-        slideElement.src = images[currentImageIndex];
-
-        slideElement.classList.add("slide-animation");
-    }, 200);
+    // Debugging: Check current image
+    console.log("Current Image: ", artistImageElement.src);
 }
 
-    // Add animation class
-    slideElement.classList.add("slide-animation");
-
-    // Delay changing the image to let the animation run smoothly
-    setTimeout(() => {
-        currentImageIndex = (currentImageIndex + 1) % images.length; // Loop back after last image
-        slideElement.src = images[currentImageIndex]; // Update image source
-        
-        // Remove animation class so it can be added again in the next cycle
-        slideElement.classList.remove("slide-animation");
-    }, 800); // Match this with the animation duration (0.8s)
-
-
-// Change image every 3 seconds
-setInterval(changeSlide, 3000);
+// Automatically change the image every 3 seconds
+setInterval(updateArtistImage, 3000);
